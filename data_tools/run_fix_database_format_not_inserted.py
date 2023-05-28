a86d7ca3ad94da8dac6119ba7d648bbaaf86a847
Matheus Borges <matheusbfaria@gmail.com>
2023-05-27 21:51:05 -0300

    Create run_fix_database_format_not_inserted.py

---
+++ data_tools/run_fix_database_format_not_inserted.py
@@ -0,0 +1,49 @@
+import re
+import sys
+import pandas as pd
+from glob import glob
+from os
+from pprint import pprint
+from time import time
+
+from database_format import DatabaseFormat
+
+sys.path.append("../utils")
+from tools import get_relevant_path
+
+sys.path.append("../odbc")
+import database_tools as dt
+
+
+def get_table_name(_str):
+    """Extract from 'database_format_not_inserted' path the table name."""
+    _str = re.search(r'(\w+)_\d+', _str).group(1)
+    table_name = re.match(r"(\w+)_\d+_\d+h", _str).group(1)
+    return table_name
+
+
+parquet_paths = glob(
+    os.path.join( 
+        get_relevant_path("database_format_not_inserted"), "*.parquet"
+    )
+)
+
+print("Parquets number: ", len(parquet_paths))
+pprint(parquet_paths)
+
+for parquet_path in parquet_paths:
+    print(f"Path: {parquet_path}")
+        
+    time1 = time()
+    
+    table_name = get_table_name(parquet_path)
+    table = pd.read_parquet(parquet_path)
+
+    database_format = DatabaseFormat(parquet_paths)
+    if_exists = "replace" if table_name in database_format.unique_value_tables else "append"
+    dataframe_not_inserted = dt.insert_database_parallel(table, table_name, if_exists=if_exists)
+    database_format.save_dataframe_not_inserted(dataframe_not_inserted, table_name)
+    
+    os.remove(parquet_path)
+    time2 = time()
+    print(f"Total time: {(time2 - time1)/60} min table_name = {table_name}, shape = {table.shape} ") 
