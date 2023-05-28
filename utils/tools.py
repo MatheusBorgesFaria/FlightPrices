cc921bd2d4398fc243d06c01ee2e138bc749ca08
Matheus Borges <mborges@localhost.localdomain>
2023-05-28 20:35:03 -0300

    Create get_structured_data_transfer_credentials() function

--- utils/tools.py
+++ utils/tools.py
@@ -3,6 +3,7 @@ from os.path import join
 
 
 PATH_TO_RELEVANT_PATHS = join("..", "settings","relevant_paths.json")
+PATH_TO_STRUTURED_DATA_TRANSFER = join("..", "settings","structured_data_transfer_credentials.json")
 
 
 def read_json(json_path=PATH_TO_RELEVANT_PATHS):
@@ -14,3 +15,8 @@ def read_json(json_path=PATH_TO_RELEVANT_PATHS):
 def get_relevant_path(key):
     """Get any key form relevant_path json."""
     return read_json(json_path=PATH_TO_RELEVANT_PATHS).get(key)
+
+
+def get_structured_data_transfer_credentials():
+    """Get structured_data_transfer_credentials json."""
+    return read_json(json_path=PATH_TO_STRUTURED_DATA_TRANSFER)
