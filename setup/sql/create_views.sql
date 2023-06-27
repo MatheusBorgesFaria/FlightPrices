69f455103b16d89986291aeb3246c856f583b331
Matheus Borges <matheusbfaria@gmail.com>
2023-06-27 19:50:37 -0300

    Create normalized prices view

--- setup/sql/create_views.sql
+++ setup/sql/create_views.sql
@@ -5,6 +5,7 @@ SELECT table_name
 FROM information_schema.tables
 WHERE table_type = 'VIEW' AND table_schema = 'flight';
 
+
 -- View of the average and standard deviation of the price of each flight segment
 CREATE VIEW flight.price_normalization_view AS
 SELECT "originCode", "destinationCode",
@@ -17,6 +18,22 @@ FROM (
         ON F."searchId" = S."searchId"
 ) subquery
 GROUP BY "originCode", "destinationCode";
+
+
+-- View of normalized prices
+CREATE VIEW flight.normalized_fares_view AS
+SELECT F."searchId", F."legId", F."totalFare",
+    (F."totalFare" - V."average") / V."standard_deviation" AS "normalizedTotalFare"
+FROM (
+    SELECT FF."searchId", FF."legId", FF."totalFare"
+    FROM flight.fare FF
+)  F
+JOIN (
+    SELECT FS."searchId", FS."originCode", FS."destinationCode"
+    FROM flight.search FS
+) S ON F."searchId" = S."searchId"
+JOIN flight.price_normalization_view V
+    ON S."originCode" = V."originCode" AND S."destinationCode" = V."destinationCode";
 
 
 -- Check existing views
